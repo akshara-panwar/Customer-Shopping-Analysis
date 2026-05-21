f4a45001bb896276622171a6f2bd4c8346389493
akshara-panwar <kahnapanwar2023@gmail.com>
2026-05-21 12:54:24 +0530

    Final customer data analysis project

--- customer_analysis.py
+++ customer_analysis.py
@@ -1,24 +1,69 @@
-# Customer Shopping Analysis
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
-## Project Overview
-This project analyzes customer shopping data using Python. It explores sales trends, customer behavior, and payment preferences.
 
-## Created By
-Akshara Panwar
+#load the dataset
+data =pd.read_csv(r'C:\Users\Akshara\Downloads\customer_data.csv')
+#display the first row of the dataset
+print('\nFirst 10 rows of the dataset:')
+print(data.head(10))
+#check for missing values
+print('\nMissing values in each column:')
+print(data.isnull().sum())
+#dataset information
+print('\nDataset Information:')
+print(data.info())
+#statistical summary of the dataset
+print('\nStatistical summary of the dataset:')
+print(data.describe())
+#customer subscriptions per year
+data['Year'] = pd.to_datetime(data['Subscription Date']).dt.year
+joined_per_year=data['Year'].value_counts().sort_index()
+print('\nNumber of customers subscribed per year:')
+print(joined_per_year)
+#plotting the number of customers subscribed per year
+plt.figure(figsize=(10,6))
+plt.bar(joined_per_year.index,joined_per_year.values,color='red')
+plt.title('Number of customers subscribed per year')
+plt.xlabel('Year')
+plt.ylabel('Number of customers')
+plt.xticks(joined_per_year.index)
+plt.grid(True,linestyle='--',alpha=0.7)
+plt.show()
+# Number of customers by company
+company_counts = data['Company'].value_counts().head(10)
 
-## Tools Used
-- Python
-- Pandas
-- Matplotlib
+print('\nTop Companies by Customer Count:')
+print(company_counts)
+#Plotting the top companies by customer count
+plt.figure(figsize=(10,6))
+plt.barh(company_counts.index,company_counts.values,color='blue')
+plt.title('Top Companies by Customer Count')
+plt.xlabel('Company')
+plt.ylabel('Number of Customers')
+plt.grid(True)
+plt.gca().invert_yaxis() 
+plt.tight_layout()
+plt.show()
 
-## Features
-- Data cleaning and preprocessing
-- Total sales calculation
-- Category-wise analysis
-- Payment method analysis
-- Monthly sales trend visualization
+#Number of customers by country
+country_counts=data['Country'].value_counts().head(10)
+print('\nTop Countries by Customer Count:')
+print(country_counts)
+#Plotting the top countries by customer count
+plt.figure(figsize=(10,6))
+plt.barh(country_counts.index,country_counts.values,color='green')
+plt.title('Top Countries by Customer Count')
+plt.xlabel('Number of Customers')
+plt.ylabel('Countries')
+plt.grid(True)
+plt.gca().invert_yaxis() 
+plt.tight_layout()
+plt.show()
 
-## Insights
-- Electronics generates highest revenue
-- Cash and UPI are frequently used payment methods
-- Sales vary across months
+
+
+
+
+
